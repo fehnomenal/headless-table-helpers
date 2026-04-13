fcff8ff159577c3cc2e48c2780be1afc3045dd17
Andreas Fehn <fehnomenal@fehn.systems>
2026-04-14 01:17:41 +0200

    fix: relax column typing on the client (#51)

--- src/client/index.svelte.ts
+++ src/client/index.svelte.ts
@@ -1,8 +1,11 @@
+import type { DataTableClientConfig } from './data-table-common.svelte.js';
 import { ClientDataTableCursor } from './data-table-cursor.svelte.js';
 import { ClientDataTableOffset } from './data-table-offset.svelte.js';
 
 export { ClientDataTableCursor, ClientDataTableOffset };
 
-export type ClientDataTable<O extends Record<string, unknown>, Column extends string> =
-  | ClientDataTableCursor<O, Column>
-  | ClientDataTableOffset<O, Column>;
+export type ClientDataTable<O extends Record<string, unknown>> =
+  | ClientDataTableCursor<O>
+  | ClientDataTableOffset<O>;
+
+export type { DataTableClientConfig };
